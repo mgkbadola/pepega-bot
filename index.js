fb202d21e7726bca3bd76a2dcbc7851ed91f5819
mgkbadola <badola3@gmail.com>
2020-06-17 15:50:50 +0530

    added yeezus and bruoh commmand along with their respective emotes

--- index.js
+++ index.js
@@ -3,12 +3,12 @@ const Discord = require('discord.js');
 const Bot = new Discord.Client();
 Bot.on('ready', ()=>{
     console.log('PepegaBot is now online.');
-})
+});
 const token = 'NzEyMTQ5MDM3NTcwMDY0NDQ0.XsOTlQ.RA0CyyZGCU-dRyuuu_C9GjVKd6w';
 
 //check whether string contains these words
 function contains(target, pattern){
-    var value = 0;
+    let value = 0;
     pattern.forEach(function(word){
         value = value + target.includes(word);
     });
@@ -17,7 +17,7 @@ function contains(target, pattern){
 
 //zulul prompts
 let z = ['zulul','allo','vj emmie','supa', 'watafak','action is coming','gwa','know da', 'commandos','i prefer men', 'poopoo',
-    'captain alex','subaru','warrior','bruce u', 'uganda', 'ugandan bruce lee', 'wakaliwood','v1', 'vi von','vi won', 'we won' ]
+    'captain alex','subaru','warrior','bruce u', 'uganda', 'ugandan bruce lee', 'wakaliwood','v1', 'vi von','vi won', 'we won' ];
 
 const prefix = '!';
 Bot.on('message', msg=>{
@@ -35,9 +35,11 @@ Bot.on('message', msg=>{
     let doggie = msg.guild.emojis.cache.get("713754840865701958").toString();
     let knioY = msg.guild.emojis.cache.get("714824198299582486").toString();
     let pepeg = msg.guild.emojis.cache.get("711091239667105842").toString();
-    let gaben = msg.guild.emojis.cache.get("714826598355828736").toString(); 
+    let gaben = msg.guild.emojis.cache.get("714826598355828736").toString();
+    let bear = msg.guild.emojis.cache.get("722749179931262997").toString();
+    let wk = msg.guild.emojis.cache.get("722754412795265106").toString();
     
-    let c= msg.content.toString()
+    let c= msg.content.toString();
 
     //commands
     if(c[0]==='!') {
@@ -45,86 +47,87 @@ Bot.on('message', msg=>{
         switch (t[0].toLowerCase()) {
             //8ball - Command under construction :D
             case 'bp':
-                msg.channel.send("Time to spend all my savings! " + pepeg + knioY + gaben + "Wise choice, my child!")
+                msg.channel.send("Time to spend all my savings! " + pepeg + knioY + gaben + " Wise choice, my child!");
                 break;
             case 'doggie':
-                msg.channel.send("I'm a DOGGIE! " + doggie)
-                let dog = new Discord.MessageAttachment('./assets/doggie.ogg')
-                msg.channel.send(dog)
+                msg.channel.send("I'm a DOGGIE! " + doggie);
+                let dog = new Discord.MessageAttachment('./assets/doggie.ogg');
+                msg.channel.send(dog);
                 break;
             case 'mc':
-                msg.channel.send('-25$ a month '+pepelook +'\t'+ mc +' I ELP SIR!')
+                msg.channel.send('-25$ a month '+pepelook +'\t'+ mc +' I ELP SIR!');
                 break;
             case '8ball':
-                msg.channel.send('Command under construction!')
+                msg.channel.send('Command under construction!');
                 break;
             case 'kekega':
-                let kekega = new Discord.MessageAttachment('./assets/kekega_lord.png')
-                msg.channel.send(kekega)
+                let kekega = new Discord.MessageAttachment('./assets/kekega_lord.png');
+                msg.channel.send(kekega);
+                break;
             case 'kekw':
             case 'etu':
-                let etu = new Discord.MessageAttachment('./assets/kekw.ogg')
-                msg.channel.send('E TU! '+ kekw)
-                msg.channel.send(etu)
+                let etu = new Discord.MessageAttachment('./assets/kekw.ogg');
+                msg.channel.send('E TU! '+ kekw);
+                msg.channel.send(etu);
                 break;
             case 'choose':
             case 'prefer':
             case 'pick':
             case 'choice':
-                let n = t.slice(1, t.length).length
+                let n = t.slice(1, t.length).length;
                 if (n <= 1) {
                     msg.channel.send("Give options you dumb ~~bitch~~ person " + forhd)
                 } else {
-                    let i = Math.floor(Math.random() * n + 1)
+                    let i = Math.floor(Math.random() * n + 1);
                     msg.channel.send("I prefer `" + `${t[i]}` + "`!")
                 }
                 break;
             case 'ome':
                 //somehow make it ignore emojis
                 msg.channel.messages.fetch({limit: 2}).then(res => {
-                    let s = res.last().toString()
-                    s = s.split("O").join(ome).split("o").join(omes)
+                    let s = res.last().toString();
+                    s = s.split("O").join(ome).split("o").join(omes);
                     msg.channel.send(s)
-                })
+                });
                 break;
             case 'allo':
-                let allo = new Discord.MessageAttachment('./assets/allo.ogg')
-                msg.react('712738743395811401')
-                msg.channel.send('ALLO! '+ zulul)
-                msg.channel.send(allo)
+                let allo = new Discord.MessageAttachment('./assets/allo.ogg');
+                msg.react('712738743395811401');
+                msg.channel.send('ALLO! '+ zulul);
+                msg.channel.send(allo);
                 break;
             case 'myiq':
-                let r = Math.floor(Math.random() * 200) + 1
+                let r = Math.floor(Math.random() * 200) + 1;
                 msg.reply("Your IQ is " + r + "!");
                 break;
             case 'leddit':
             case 'reddit':
-                msg.channel.send("https://www.reddit.com/user/topachez")
+                msg.channel.send("https://www.reddit.com/user/topachez");
                 break;
             case 'youtube':
             case 'yt':
-                msg.channel.send("https://www.youtube.com/channel/UC7Zqy9v4kENk5N5hBfo3Bog")
+                msg.channel.send("https://www.youtube.com/channel/UC7Zqy9v4kENk5N5hBfo3Bog");
                 break;
             case 'soundcloud':
-                msg.channel.send("https://soundcloud.com/topachez")
+                msg.channel.send("https://soundcloud.com/topachez");
                 break;
             case 'pepeg':
             case 'pepega':
             case 'pepege':
-                const admiral_pepeg = new Discord.MessageAttachment('./assets/pepeg.ogg')
-                msg.channel.send(admiral_pepeg)
+                const admiral_pepeg = new Discord.MessageAttachment('./assets/pepeg.ogg');
+                msg.channel.send(admiral_pepeg);
                 break;
             case 'userinfo':
-                let user = msg.mentions.users.first()
-                let member = msg.mentions.members.first()
+                let user = msg.mentions.users.first();
+                let member = msg.mentions.members.first();
                 if (!user) {
                     user = msg.author
                 }
                 if (!member) {
                     member = msg.member
                 }
-                var status = user.presence.status
-                status = status.charAt(0).toUpperCase() + status.slice(1)
+                var status = user.presence.status;
+                status = status.charAt(0).toUpperCase() + status.slice(1);
                 const info = new Discord.MessageEmbed()
                     .setTitle('USER INFORMATION')
                     .setColor(0xe1e1e1)
@@ -135,19 +138,32 @@ Bot.on('message', msg=>{
                     .addField('Roles', member.roles.cache.map(r => `${r}`).join(' | '), true);
                 msg.channel.send(info);
                 break;
+            case 'yeezus':
+                msg.channel.send('who is yeezus?:rolling_eyes::face_with_hand_over_mouth::thinking::yawning_face:\n' +
+                    '\n' +
+                    'in math: pedo'+ bear + bear + '\n' +
+                    'in history: pedo'+ bear + bear + '\n' +
+                    'in art :pedo'+ bear + bear + '\n'+
+                    'in science: pedo'+ bear + bear + '\n' +
+                    'in geography: pedo'+ bear + bear + '\n');
+                msg.channel.send('Just kidding, you kinda cute ngl \n   :flushed:\n:point_right::point_left:');
+                break;
+            case 'bruoh':
+                msg.channel.send('Bruoh\'s cheerleader WK = insta win ' + wk);
+                break;
         }
     }
 
     //reactions triggers
     else{
         if(contains(c.toLowerCase(),z))
-            msg.react('712738743395811401')
+            msg.react('712738743395811401');
         if(c.includes('more like')){
-            msg.react('712180552408760351')
-            msg.react('713344557575700530')
+            msg.react('712180552408760351');
+            msg.react('713344557575700530');
             msg.channel.send(`React to tell whether it was ${pog} or ${ppscoots}`)
         }
     }
-})
+});
 //login
 Bot.login(token);
