32cb9114f6fbb9777d00fefb091ca2e2418596ed
Mrigank Badola <badola3@gmail.com>
2020-11-30 12:40:25 +0530

    updated the method of fetching emotes and cleaned up code

--- functions.js
+++ functions.js
@@ -1,4 +1,11 @@
 module.exports = {
+    contains: function (target, pattern) {
+        let value = 0;
+        pattern.forEach(function (word) {
+            value = value + target.includes(word);
+        });
+        return value >= 1;
+    },
     twitch: function (channel) {
         let link = "https://www.twitch.tv/";
         switch (channel) {
@@ -48,10 +55,10 @@ module.exports = {
             orange[4] + orange[5] + orange[6] + "\n" +
             orange[7] + orange[8] + orange[9])
     },
-    choose: function (options, fourhead) {
+    choose: function (options, yeahbhai) {
         let n = options.slice(1, options.length).length;
         if (n <= 1) {
-            return (`Give options you dummy! ${fourhead}`);
+            return (`Give options you dummy! ${yeahbhai}`);
         } else {
             let i = Math.floor(Math.random() * n + 1);
             return (`I would prefer ${options[i]}!`);
@@ -61,29 +68,14 @@ module.exports = {
         let r = Math.floor(Math.random() * 200) + 1;
         return (`Your IQ is ${r}!`);
     },
-    yeezus: function (bear) {
-        return ("who is yeezus?:rolling_eyes::face_with_hand_over_mouth::thinking::yawning_face:\n" +
-            "\n" +
-            "in math: pedo" +
-            bear +
-            bear +
-            "\n" +
-            "in history: pedo" +
-            bear +
-            bear +
-            "\n" +
-            "in art :pedo" +
-            bear +
-            bear +
-            "\n" +
-            "in science: pedo" +
-            bear +
-            bear +
-            "\n" +
-            "in geography: pedo" +
-            bear +
-            bear +
-            "\n"
-        )
-    }
+    commands: ["8ball", "allo", "avatar", "bdog", "bp", "brazil", "bruoh", "bulldog", "choose", "doggie", "elp", "etu",
+        "help", "kekega", "kektff", "kekw", "leddit", "mc", "myiq", "ome", "orangey", "pandit", "pepeg", "pepega",
+        "pepege", "prefer", "reddit", "shadap", "showall", "soundcloud", "sourdough", "starter", "tank", "twitch",
+        "userinfo", "youtube", "yt"],
+    z: [
+        "zulul", "allo", "vj emmie", "supa", "watafak", "action is coming", "gwa", "know da", "commando",
+        "i prefer men", "poopoo", "poo poo", "captain alex", "subaru", "warria", "bruce u", "uganda",
+        "ugandan bruce lee", "wakaliwood", "v1", "vi von", "vi won", "we won", "vroom", "expect the unexpectable",
+        "y are u gae", "🇺🇬", "brazil"
+    ]
 }
---
+++ messageHandle.js
@@ -0,0 +1,326 @@
+const Discord = require("discord.js");
+const fun = require('./functions')
+module.exports =
+    {
+        messageHandler: function (msg, Bot) {
+            //emotes
+            let orange = [];
+            orange[0] = Bot.emojis.cache.find(emote => emote.name === 'Flushed_Orange').toString();
+            for (let i = 1; i <= 9; i++)
+                orange[i] = Bot.emojis.cache.find(emote => emote.name === `Orange${i}`).toString();
+
+            let kekega = [];
+            kekega[0] = Bot.emojis.cache.find(emote => emote.name === `Kekega`).toString();
+            for (let i = 1; i <= 9; i++)
+                kekega[i] = Bot.emojis.cache.find(emote => emote.name === `KEKEGALord${i}`).toString();
+
+            let kektff = [];
+            kektff[0] = Bot.emojis.cache.find(emote => emote.name === 'KEKTFF').toString();
+            for (let i = 1; i <= 9; i++)
+                kektff[i] = Bot.emojis.cache.find(emote => emote.name === `KEKTFF${i}`).toString();
+
+            let gun = [];
+            gun[0] = Bot.emojis.cache.find(emote => emote.name === 'gunL').toString();
+            gun[1] = Bot.emojis.cache.find(emote => emote.name === 'gunR').toString();
+
+            let omegaluls = [];
+            omegaluls[0] = Bot.emojis.cache.find(emote => emote.name === 'OMEGALUL').toString(); //OMEGALUL
+            omegaluls[1] = Bot.emojis.cache.find(emote => emote.name === 'smolOME').toString(); //smolOME
+
+            let fat = [];
+            fat[0] = Bot.emojis.cache.find(emote => emote.name === 'FatHead').toString();
+            fat[1] = Bot.emojis.cache.find(emote => emote.name === 'FatTorso').toString();
+            fat[2] = Bot.emojis.cache.find(emote => emote.name === 'FatLegs').toString();
+
+            let pointup = [];
+            pointup[0] = Bot.emojis.cache.find(emote => emote.name === 'invis').toString();
+            for (let i = 1; i <= 7; i++)
+                pointup[i] = Bot.emojis.cache.find(emote => emote.name === `pointup${i}`).toString();
+
+            let yeahbhai = Bot.emojis.cache.find(emote => emote.name === 'YeahBhai').toString();
+            let zulul = Bot.emojis.cache.find(emote => emote.name === 'ZULUL').toString();
+            let kekw = Bot.emojis.cache.find(emote => emote.name === 'KEKW').toString();
+            let pepelook = Bot.emojis.cache.find(emote => emote.name === 'pepeLook').toString();
+            let mc = Bot.emojis.cache.find(emote => emote.name === 'admiralMC').toString();
+            let doggie = Bot.emojis.cache.find(emote => emote.name === 'DogegeJAM').toString();
+            let knioY = Bot.emojis.cache.find(emote => emote.name === 'knioY').toString();
+            let pepeg = Bot.emojis.cache.find(emote => emote.name === 'Pepeg').toString();
+            let gaben = Bot.emojis.cache.find(emote => emote.name === 'GabeN').toString();
+            let wk = Bot.emojis.cache.find(emote => emote.name === 'bruohWK').toString();
+            let pandit = Bot.emojis.cache.find(emote => emote.name === 'Panditga').toString();
+            let monkaUGun = Bot.emojis.cache.find(emoji => emoji.name === 'monkaUGun');
+
+            let c = msg.content.toString();
+
+            //fun.commands
+            if (c[0] === "!") {
+                let t = msg.content.substring(1).split(" ");
+                switch (t[0].toLowerCase()) {
+                    case fun.commands[21]:
+                    case fun.commands[0]:
+                        msg.channel.send(pandit).then(() => {
+                        });
+                        if (t.length === 1)
+                            msg.channel.send(`Predict what?`).then(() => {
+                            })
+                        else {
+                            let options = [
+                                "Feeling uncertain. Try Again.",
+                                "It seems like it.",
+                                "No.",
+                                "My prediction is in positive light.",
+                                "Negative remarks on that thought.",
+                                "Please try again.",
+                                "Yes.",
+                                "Most certainly.",
+                                "My sources say no.",
+                                "Affirmative.",
+                                "Negative.",
+                                "Yep.",
+                                "Nope.",
+                                "Can't decide at the moment.",
+                                "My brain is lagging, try to ask again.",
+                                "Heavily unlikely.",
+                            ];
+                            let index = Math.floor(Math.random() * options.length);
+                            msg.channel.send("`" + options[index] + "`").then(() => {
+                            });
+                        }
+                        break;
+                    case fun.commands[1]:
+                        let allo = new Discord.MessageAttachment("./assets/allo.ogg");
+                        msg.channel.send("ALLO! " + zulul).then(() => {
+                        });
+                        msg.channel.send(allo).then(() => {
+                        });
+                        break;
+                    case fun.commands[2]:
+                        let uzer = msg.mentions.users.first();
+                        if (!uzer)
+                            uzer = msg.author
+                        msg.channel.send(uzer.displayAvatarURL({dynamic: true})).then(() => {
+                        })
+                        break
+                    case fun.commands[7]:
+                    case fun.commands[3]:
+                        msg.channel.send(fat[0] + "\n" + fat[1] + "\n" + fat[2]).then(() => {
+                        });
+                        let heyguys = new Discord.MessageAttachment("./assets/heyguys.ogg");
+                        msg.channel.send(heyguys).then(() => {
+                        });
+                        break;
+                    case fun.commands[4]:
+                        msg.channel.send(fun.battlepass(pepeg, knioY, gaben)).then(() => {
+                        })
+
+                        break;
+                    case fun.commands[5]:
+                        msg.reply(`Welcome to Brazil ${monkaUGun}`)
+                        break;
+                    case fun.commands[6]:
+                        msg.channel.send("Bruoh's cheerleader WK = insta win " + wk).then(() => {
+                        });
+                        break;
+                    case fun.commands[8]:
+                    case fun.commands[25]:
+                        msg.channel.send(fun.choose(t, yeahbhai)).then(() => {
+                        })
+                        break;
+                    case fun.commands[9]:
+                        msg.channel.send(`I'm a DOGGIE! ${doggie}`).then(() => {
+                        });
+                        let dog = new Discord.MessageAttachment("./assets/doggie.ogg");
+                        msg.channel.send(dog).then(() => {
+                        });
+                        break;
+                    case fun.commands[10]:
+                    case fun.commands[12]:
+                        const command_array = new Discord.MessageEmbed()
+                            .setTitle("Help")
+                            .setColor(0x696969)
+                            .addField("Commands List", `\`${fun.commands.join("`, `")}\``, true);
+                        msg.channel.send(command_array).then(() => {
+                        });
+                        break;
+                    case fun.commands[15]:
+                    case fun.commands[11]:
+                        let etu = new Discord.MessageAttachment("./assets/kekw.ogg");
+                        msg.channel.send("E TU! " + kekw).then(() => {
+                        });
+                        msg.channel.send(etu).then(() => {
+                        });
+                        break;
+                    case fun.commands[13]:
+                        msg.channel.send(gun[0] + kekega[0] + gun[1]).then(() => {
+                        })
+
+                        msg.channel.send(fun.kekega_lord(kekega)).then(() => {
+                        })
+
+                        break;
+                    case fun.commands[14]:
+                        msg.channel.send(pointup[0] + kektff[0] + "\t\t\t\t\t👇").then(() => {
+                        })
+                        msg.channel.send(fun.bigtff(kektff, pointup)).then(() => {
+                        })
+
+                        break;
+                    case fun.commands[16]:
+                    case fun.commands[26]:
+                        msg.channel.send("https://www.reddit.com/user/topachez").then(() => {
+                        })
+
+                        break;
+                    case fun.commands[17]:
+                        msg.channel.send(fun.megasub(pepelook, mc)).then(() => {
+                        })
+
+                        break;
+                    case fun.commands[18]:
+                        msg.reply(fun.myiq())
+
+                        break;
+                    case fun.commands[19]:
+                        //somehow make it ignore emojis
+                        msg.channel.messages.fetch({limit: 2}).then((res) => {
+                            let s = res.last().toString();
+                            s = s.split("O").join(omegaluls[0]).split("o").join(omegaluls[1]);
+                            msg.channel.send(s).then(() => {
+                            });
+                        });
+                        break;
+                    case fun.commands[20]:
+                        msg.channel.send(pepelook + "\t" + orange[0]).then(() => {
+                        });
+                        msg.channel.send(fun.orangey(orange)).then(() => {
+                        });
+                        break;
+                    case fun.commands[22]:
+                    case fun.commands[23]:
+                    case fun.commands[24]:
+                        const admiral_pepeg = new Discord.MessageAttachment(
+                            "./assets/pepeg.ogg"
+                        );
+                        msg.channel.send(admiral_pepeg).then(() => {
+                        });
+                        break;
+                    case fun.commands[27]:
+                        let shadap = new Discord.MessageAttachment("./assets/shadap.wav");
+                        msg.channel.send(shadap).then(() => {
+                        });
+                        break;
+                    case fun.commands[28]:
+                        let count = 0
+                        let emotearray = msg.guild.emojis.cache.map(e => `${e.toString()}`)
+                        let tempstack = []
+                        for (let i = 0; i < emotearray.length; i++) {
+                            count = count + emotearray[i].length
+                            if (count >= 2000 || i === emotearray.length - 1) {
+                                msg.channel.send(tempstack.join("")).then(() => {
+                                });
+                                tempstack = []
+                                count = emotearray[i].length
+                            }
+                            tempstack.push(emotearray[i])
+                        }
+                        break;
+                    case fun.commands[29]:
+                        msg.channel.send("https://soundcloud.com/topachez").then(() => {
+                        })
+
+                        break;
+                    case fun.commands[30]:
+                    case fun.commands[31]:
+                        if (msg.guild.name === "Gush Panda's Baking Club")
+                            msg.member.send(
+                                "All of my followers (in the future Subscribers) will get access to my " +
+                                "sourdough-recipes channel and more importantly, an exclusive help and guidance directly from " +
+                                "me in the `#starter-making-help` channel: <https://discord.gg/Kzb4eug>.\n" +
+                                "~**Gush Panda**"
+                            ).then(() => {
+                            });
+                        break;
+                    case fun.commands[32]:
+                        if (t.length === 1) {
+                            msg.channel.send(`
+.......................██▄▄▄▄▄▄▄▄▄▄▄▄▄
+........▂▄▅█████████▅▄▃▂
+........ ████████████████████
+........◥⊙▲⊙▲⊙▲⊙▲⊙▲⊙▲⊙◤`).then(() => {
+                            });
+                        } else if (t.length === 2) {
+                            msg.channel.send(`
+.......................█ ${t[1]} █▄▄▄▄▄▄▄▄▄▄▄▄▄ ➢➣➢➣➢➣
+........▂▄▅█████████▅▄▃▂
+........ ████████████████████
+........◥⊙▲⊙▲⊙▲⊙▲⊙▲⊙▲⊙◤`).then(() => {
+                            });
+                        } else if (t.length === 3) {
+                            msg.channel.send(`
+.......................█ ${t[1]} █▄▄▄▄▄▄▄▄▄▄▄▄▄ ➢➣➢➣➢➣ ${t[2]}
+........▂▄▅█████████▅▄▃▂
+........ ████████████████████
+........◥⊙▲⊙▲⊙▲⊙▲⊙▲⊙▲⊙◤`).then(() => {
+                            });
+                        }
+                        break
+                    case fun.commands[33]:
+                        msg.channel.send(fun.twitch(msg.guild.name)).then(() => {
+                        })
+                        break;
+                    case fun.commands[34]:
+                        let user = msg.mentions.users.first();
+                        let member = msg.mentions.members.first();
+                        if (!user)
+                            user = msg.author;
+
+                        if (!member)
+                            member = msg.member;
+
+                        let flag = true;
+                        if (member.nickname === null)
+                            flag = false;
+
+                        let author = user.username + "#" + user.discriminator;
+                        if (flag)
+                            author.concat(" aka " + member.nickname);
+
+                        let roles = member.roles.cache.map((r) => r);
+                        let color = parseInt(roles[0].hexColor.replace("#", "0x"));
+                        if (roles[0].name !== "everyone") roles.pop();
+
+                        let status = user.presence.status;
+                        status = status.charAt(0).toUpperCase() + status.slice(1);
+                        const info = new Discord.MessageEmbed()
+                            .setTitle("USER INFORMATION")
+                            .setColor(color)
+                            .setThumbnail(user.displayAvatarURL({dynamic: true}))
+                            .setAuthor(author)
+                            .addField("Status", `Being a ${pepeg} in \`${status}\`  Mode`)
+                            .addField("Roles", roles.join(" | "), true);
+                        msg.channel.send(info).then(() => {
+                        })
+
+                        break;
+                    case fun.commands[35]:
+                    case fun.commands[36]:
+                        msg.channel.send("https://www.youtube.com/channel/UC7Zqy9v4kENk5N5hBfo3Bog").then(() => {
+                        })
+
+                        break;
+                }
+            } else {
+                if (fun.contains(c.toLowerCase(), fun.z))
+                    msg.react("712738743395811401").then(() => {
+                    });
+
+                if (c.includes("more like")) {
+                    msg.react("746646238354735164").then(() => {
+                    });
+                    msg.react("713344557575700530").then(() => {
+                    });
+                }
+            }
+        },
+        Discord
+    }
